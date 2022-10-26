d44957bfec4001ed1596824c9571c3775736b67f
alan.yushu.zhang@gmail.com <alan.yushu.zhang@gmail.com>
2022-10-26 14:57:33 -0600

    added addSongToPlaylist function

--- songActions.py
+++ songActions.py
@@ -1,4 +1,5 @@
 import sqlite3
+from createPlaylist import createPlaylist
 import startSession
 conn = sqlite3.connect('./a2.db')
 
@@ -46,6 +47,8 @@ names of artists who performed it in addition to
 id, title and duration of the song as well as the 
 names of playlists the song is in (if any)'''
 def songInfo(sid):
+    # beta test works
+    # need to add later: see if sid input is valid sid
     songInfo = '''
     SELECT DISTINCT a.name
     FROM (songs s LEFT OUTER JOIN perform p ON s.sid = p.sid)
@@ -74,7 +77,39 @@ def songInfo(sid):
     print("Playlists", songDetail[0][1], "is in:")
     for playlist in playlists:
         print(playlist[0])
+#songInfo(10)
 
+def addSongToPlaylist(uid, sid):
+    # beta test works
+    getPlaylists = '''
+    SELECT p.pid, p.title
+    FROM playlists p
+    WHERE p.uid = :uid;
+    '''
+    c.execute(getPlaylists, {'uid':uid})
+    playlists = c.fetchall()
+    if playlists==[]:
+        print("You have no playlists")
+        createPlaylist(uid)
+    else:
+        print("Here are your playlists")
+        for playlist in playlists:
+            print(playlist[0], playlist[1])
+        pid = input("Enter the pid of playlist you wish to add the song to: ")
+        # add later: need to verify that pid is valid and belongs to user
+        # add later: need to check if song already in playlist
+        getSorder = '''
+        SELECT MAX(pl.sorder)+1
+        FROM plinclude pl
+        WHERE pl.pid = :pid;'''
+        c.execute(getSorder, {'pid': pid})
+        sorder = c.fetchone()[0]
+        insertIntoPlaylist = '''
+        INSERT INTO plinclude VALUES (:pid, :sid, :sorder);
+        '''
+        c.execute(insertIntoPlaylist, {'pid': pid, 'sid': sid, 'sorder': sorder})
+        conn.commit()
+        print("Song", sid, "added to playlist", pid, "at position", sorder)
 
-songInfo(10)
+#addSongToPlaylist('u25', 26)
 conn.close()
