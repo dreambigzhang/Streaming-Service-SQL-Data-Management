153e682c5c05c8de459d20453c31581c92d24e54
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-10-27 17:59:27 -0600

    Update signup.py

--- signup.py
+++ signup.py
@@ -1,1 +1,26 @@
+import sqlite3
+import getpass
 
+conn = sqlite3.connect('./a2.db')
+
+c = conn.cursor()
+
+def signup(): 
+    userID = None
+    while userID == None or len(userID) > 20: 
+
+        userID = input("Input a unique ID: ")
+        pwd = getpass.getpass(prompt='Enter your password: ', stream= None)
+        # check if unique ID
+        c.execute('''
+            SELECT uid
+            FROM users  
+            WHERE uid = ?'''
+            ,(userID))
+        validNewID = c.rowcount()
+        if validNewID == 0: 
+            print("ID is not unique")
+            userID = None  # while loop continues
+
+
+    c.execute("""INSERT INTO users VALUE (?,?)""",(userID,userPWD))
