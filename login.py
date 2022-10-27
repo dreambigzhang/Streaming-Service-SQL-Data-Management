1c5ea7b53b3a87f10509c9e505e54ef74500125e
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-10-27 17:49:09 -0600

    returns userID and artistID now

--- login.py
+++ login.py
@@ -5,10 +5,10 @@ conn = sqlite3.connect('./a2.db')
 
 c = conn.cursor()
 
-def login(): 
-    validUser = False
-    validArtist = False
-    while not validUser or not validArtist: 
+def login(): # returns userID and artistID, at least one is not None
+    userID = None
+    artistID = None
+    while userID == None and artistID == None: 
             #take ID and password
             ID = input("Enter ID: ")
             pwd = getpass.getpass(prompt='Enter your password: ', stream= None)
@@ -17,27 +17,25 @@ def login():
             c.execute('''
                 SELECT u.uid 
                 FROM users u
-                WHERE uid = ? and pwd = ?'''
+                WHERE uid = ? and pwd = ?;'''
                 ,(ID,pwd))
             
             row = c.fetchone()
             if c.rowcount() != 0:
                 userID = row[0]
-                validUser = True
         
             #check if valid artist
             c.execute('''
                 SELECT a.aid 
                 FROM artists  
-                WHERE uid = ? and pwd = ?'''
+                WHERE uid = ? and pwd = ?;'''
                 ,(ID,pwd))
             
             row = c.fetchone()
             if c.rowcount() != 0:
                 artistID = row[0]
-                validArtist = True
-            if not validUser and not validArtist:
+            if userId == None and artistId == None:
                 print("Not a valid username or password")
-    return validUser, ValidArtist
+    return userID, artistID
                 
 
