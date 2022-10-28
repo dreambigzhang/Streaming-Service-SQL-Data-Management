cbe21976871b69e1ee09bed5ceaa20497b1c2b02
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-10-27 23:28:22 -0600

    takes name of database and returns it

---
+++ dbEntry.py
@@ -0,0 +1,5 @@
+# we need to check for valid database entry (NOT DONE YET)
+def dbEntry():
+    # should returns correct name of database
+    db = input("Input name of database file")
+    return db
