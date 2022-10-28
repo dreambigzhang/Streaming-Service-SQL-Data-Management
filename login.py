066b3f464b088b3e81ac626c7d9ac186f2705bfc
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-10-27 22:37:36 -0600

    Update login.py

--- login.py
+++ login.py
@@ -13,29 +13,32 @@ def login(): # returns userID and artistID, at least one is not None
             ID = input("Enter ID: ")
             pwd = getpass.getpass(prompt='Enter your password: ', stream= None)
             
-            #check if valid user
+            # query all users matching ID and pwd
             c.execute('''
                 SELECT u.uid 
                 FROM users u
-                WHERE uid = ? and pwd = ?;'''
+                WHERE u.uid = ? and u.pwd = ?;'''
                 ,(ID,pwd))
             
+            # check if valid user ID
             row = c.fetchone()
-            if c.rowcount() != 0:
+            if row is not None:
                 userID = row[0]
         
-            #check if valid artist
+            # query all artists matching ID and pwd
             c.execute('''
                 SELECT a.aid 
-                FROM artists  
-                WHERE uid = ? and pwd = ?;'''
+                FROM artists a 
+                WHERE a.aid = ? and a.pwd = ?;'''
                 ,(ID,pwd))
             
+            # check if valid user ID
             row = c.fetchone()
-            if c.rowcount() != 0:
+            if row is not None:
                 artistID = row[0]
+            
+            # if incorrect ID and pwd tell user and redo loop
             if userID == None and artistID == None:
                 print("Not a valid username or password")
     return userID, artistID
-                
-
+               
