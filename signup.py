19a4cbe38768505bc73d93e9d4b2ce6a998bc6ae
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-10-27 23:11:26 -0600

    signs up the user to the database

--- signup.py
+++ signup.py
@@ -11,16 +11,17 @@ def signup():
 
         userID = input("Input a unique ID: ")
         pwd = getpass.getpass(prompt='Enter your password: ', stream= None)
+        name = input("What is your name? ")
         # check if unique ID
         c.execute('''
-            SELECT uid
-            FROM users  
-            WHERE uid = ?'''
-            ,(userID))
-        validNewID = c.rowcount()
-        if validNewID == 0: 
+            SELECT u.uid
+            FROM users u 
+            WHERE u.uid = ?;'''
+            ,(userID,))
+        validNewID = c.fetchone()
+        if validNewID is not None: 
             print("ID is not unique")
             userID = None  # while loop continues
 
 
-    c.execute("""INSERT INTO users VALUE (?,?)""",(userID,userPWD))
+    c.execute("""INSERT INTO users VALUES (?,?,?);""",(userID,name,pwd))
