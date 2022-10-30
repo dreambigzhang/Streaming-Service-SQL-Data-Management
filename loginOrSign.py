8db05a7e837db23f27308230b8c44f3443c74915
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-10-29 22:19:58 -0600

    added file to get user choice in main screen

---
+++ loginOrSign.py
@@ -0,0 +1,9 @@
+def loginOrSign(): 
+    # get user input on whether to login or sign up
+    mainMenuAction = input("Enter\n1 to login\n2 to sign up \nAnything else to exit program\n")
+    mainMenuAction = mainMenuAction.strip()  # remove any unneccessary spaces
+
+    print(mainMenuAction)
+    return mainMenuAction  # returns 1 for login, 2 for sign up, all else for exit
+
+loginOrSign()
