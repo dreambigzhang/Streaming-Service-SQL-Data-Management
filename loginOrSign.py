3fffd3c1786d7c07957ddae7add1da99b8516000
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-10-29 22:33:37 -0600

    Update loginOrSign.py

--- loginOrSign.py
+++ loginOrSign.py
@@ -3,5 +3,4 @@ def loginOrSign():
     mainMenuAction = input("Enter\n1 to login\n2 to sign up \nAnything else to exit program\n")
     mainMenuAction = mainMenuAction.strip()  # remove any unneccessary spaces
 
-    print(mainMenuAction)
     return mainMenuAction  # returns 1 for login, 2 for sign up, all else for exit
