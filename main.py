20030faa4c9b96dbd88e3feafcb5985faaccb112
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-10-29 23:23:52 -0600

    created a main function
    conn is made as a global variable so that when main is called again, it doesn't ask for database name again

---
+++ main.py
@@ -0,0 +1,49 @@
+import sqlite3
+from dbEntry import dbEntry
+from loginOrSign import loginOrSign
+from login import login
+from signup import signup
+from userMainMenu import userMainMenu
+conn = dbEntry()
+
+def main(): 
+      # returns name of database
+
+    initialAction = loginOrSign() # 1 for login, 2 for sign up, all else for exit
+    uid = None 
+    aid = None
+    if initialAction == '1':
+        uid,aid = login(conn)  # if 
+    elif initialAction == '2': 
+        uid = signup(conn)
+    else: 
+        return  # exit program
+
+    if uid == None and aid == None:
+        main() 
+        return
+    elif uid == aid: 
+        action = input("Enter 1 to go into your user account or 2 to enter into your artist account: ").strip()
+        while action != '1' and action != '2': 
+            print("Invalid action, try again")
+            action = input("Enter 1 to go into your user account or 2 to enter into your artist account: ").strip()
+        
+        if action == '1': 
+            possibleLogout = userMainMenu(uid,conn)
+            if possibleLogout == '5': 
+                main()
+                return
+        else: 
+            # put artist main menu here
+            return
+    elif uid != None: 
+        possibleLogout = userMainMenu(uid,conn)
+        if possibleLogout == '5':
+            main()
+            return
+    else: 
+        # put artist main menu here
+        return
+    
+
+main()
