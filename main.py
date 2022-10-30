76f4ee9c857a4598748622dab67c8cf9d424de69
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-10-29 23:26:46 -0600

    added comments

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def main():
     uid = None 
     aid = None
     if initialAction == '1':
-        uid,aid = login(conn)  # if 
+        uid,aid = login(conn)  # if both stay None, user is backing out to loginOrSign function again
     elif initialAction == '2': 
-        uid = signup(conn)
+        uid = signup(conn) # signing up user
     else: 
         return  # exit program
 
-    if uid == None and aid == None:
+    if uid == None and aid == None:  # means to back out to main screen again
         main() 
         return
     elif uid == aid: 
@@ -29,7 +29,7 @@ def main():
             action = input("Enter 1 to go into your user account or 2 to enter into your artist account: ").strip()
         
         if action == '1': 
-            possibleLogout = userMainMenu(uid,conn)
+            possibleLogout = userMainMenu(uid,conn) # userMainMenu returns 5 if logout, returns None otherwise
             if possibleLogout == '5': 
                 main()
                 return
