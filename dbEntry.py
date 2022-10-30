bc553ad51c7abaea61d05d84b992c8cb18661736
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-10-29 22:38:15 -0600

    Update dbEntry.py

--- dbEntry.py
+++ dbEntry.py
@@ -2,6 +2,6 @@ import sqlite3
 
 def dbEntry():
     # should returns correct name of database
-    db = input("Input name of database file")
+    db = input("Input name of database file: ")
     conn = sqlite3.connect('./' + db)
     return conn
