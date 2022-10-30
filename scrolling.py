86f7e0995e17a9c3b075e34214c6b0ca80a17b27
alan.yushu.zhang@gmail.com <alan.yushu.zhang@gmail.com>
2022-10-30 14:06:51 -0600

    added the ability to return True to return to the main menu

--- scrolling.py
+++ scrolling.py
@@ -1,8 +1,9 @@
 # import only system from os
 
 from os import system, name
-
+from playlistInfo import playlistInfo
 from songActions import songActions
+
 def clear(): # need to test this works on lab machine
     # for windows
     if name == 'nt':
@@ -25,22 +26,20 @@ def showFive(list1, i):
         i = i % listLen
     
     clear()
-    currentlyDisplayedID = []
     j = i
     print("---Search Result---")
     print("id   |   title   |   duration   |   type")
     while j<i+5 and j<listLen:
-        currentlyDisplayedID.append(list1[j][0])
         print(list1[j][0:-1])
         j+=1
-    return currentlyDisplayedID
+    return
 def scrolling(uid,list1, i, conn):
     c = conn.cursor()
     if i < 0:
         i = 0
     if i+5 > len(list1):
         i = len(list1)-5
-    currentlyDisplayedID = showFive(list1, i)
+    showFive(list1, i)
     action = input("Enter:\nw to scroll up\ns to scroll down\n1 to select a song\n2 to select a playlist\nanything else to return to the main menu: ").lower()
     if action=='w':
         if scrolling(uid, list1, i-5, conn) == True:
@@ -49,7 +48,14 @@ def scrolling(uid,list1, i, conn):
         if scrolling(uid, list1, i+5, conn) == True:
             return True 
     elif action == '1':
-        sid = input("Enter the song's id")
-        songActions(uid, sid, conn)
+        sid = input("Enter the song's id: ")
+        if songActions(uid, sid, conn) == True: # return True to return to the main menu
+            return True
+    elif action == '2':
+        pid = input("Enter the playlist's id: ")
+        if playlistInfo(uid, pid, conn)==True:
+            return True
+    else:
+        return True
 
 #scrolling(list1, 0) # testing purpose
