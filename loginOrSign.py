787e31b998a04f071487c55d92f833950e723720
alan.yushu.zhang@gmail.com <alan.yushu.zhang@gmail.com>
2022-11-01 23:53:45 -0600

    modify the prompt to specify that signing up is only for user

--- loginOrSign.py
+++ loginOrSign.py
@@ -1,6 +1,6 @@
 def loginOrSign(): 
     # get user input on whether to login or sign up
-    mainMenuAction = input("Enter\n1 to login\n2 to sign up \nAnything else to exit program\n")
+    mainMenuAction = input("Enter\n1 to login\n2 to sign up as a new user\nAnything else to exit program\n")
     mainMenuAction = mainMenuAction.strip()  # remove any unneccessary spaces
 
     return mainMenuAction  # returns 1 for login, 2 for sign up, all else for exit
