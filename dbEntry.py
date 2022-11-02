5258b451129e8e37c799fdf97317f48ce8f314c6
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-11-01 22:32:21 -0600

    handled case user doesnt provide command line name

--- dbEntry.py
+++ dbEntry.py
@@ -2,6 +2,9 @@ import sqlite3
 import sys
 def dbEntry():
     # should returns correct name of database
-    db = sys.argv[1]
+    try:
+        db = sys.argv[1]
+    except: 
+        db = input("Input the name of the database to be used: ")
     conn = sqlite3.connect('./' + db)
     return conn
