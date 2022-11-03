c7c4b960820bddf2a7227aa81c615584abb4490b
alan.yushu.zhang@gmail.com <alan.yushu.zhang@gmail.com>
2022-11-02 23:26:24 -0600

    return the recursive calls of userMainMenu and let the return value bubble up

--- userMainMenu.py
+++ userMainMenu.py
@@ -15,34 +15,35 @@ def clear(): # need to test this works on lab machine
         _ = system('clear')
 
 def userMainMenu(uid, conn):
-    clear()
+    #clear()
     print("------User Main Menu-------")
     action = input("Enter\n1 to start session\n2 to end session\n3 to search for songs and playlists\n4 to search for artists\n5 to logout\nAnything else to exit program \n").strip()
     if action=='1':
         clear()
         startSession(uid, conn)
-        userMainMenu(uid, conn)
+        return userMainMenu(uid, conn)
     elif action == '2':
         clear()
         endSession(uid, conn)
-        userMainMenu(uid, conn)
+        return userMainMenu(uid, conn)
     elif action == '3':
         clear()
         if searchSongsAndPlaylists(uid, conn)==True:
-            userMainMenu(uid, conn)
+            return userMainMenu(uid, conn)
     elif action == '4':
         clear()
         if searchArtists(uid, conn)== True:
-            userMainMenu(uid, conn)
+            return userMainMenu(uid, conn)
     elif action == '5':
+        endSession(uid, conn)
         clear()
         print("logged out")
         return '5' # should return to main screen
-    else: 
+    else:
+        endSession(uid, conn)
         clear()
-        endSession(uid,conn)
         return -1
-    return 
+    return
 
 if __name__ == "__main__":
     conn = sqlite3.connect('./new.db')
